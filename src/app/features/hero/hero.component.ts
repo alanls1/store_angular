d28af71647ed235ccabab85aa888b22e90092281
alanls1 <alanlima4123@gmail.com>
2025-06-13 13:51:14 -0300

    fix: Fixing the layout

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../share/card/card.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  items = [1, 2, 3, 4, 5, 6, 7];
 }
