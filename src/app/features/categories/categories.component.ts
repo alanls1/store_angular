8429abc0320c5576135555588e4ddbfd047ec22d
alanls1 <alanlima4123@gmail.com>
2025-06-17 11:10:03 -0300

    feat: section category

---
+++ src/app/features/categories/categories.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../share/button/button.component';
+
+@Component({
+  selector: 'app-categories',
+  imports: [ButtonComponent],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss',
+})
+export class CategoriesComponent {
+  cards = [
+    {
+      title: 'Roupas',
+      p: 'Estilo e conforto para todos os momentos',
+      button: 'Explorar Roupas',
+      class: 'clothes',
+    },
+    {
+      title: 'Joias',
+      p: 'Brilho e elegância para completar seu visual',
+      button: 'Explorar Joias',
+      class: 'jewelry',
+    },
+  ];
+}
