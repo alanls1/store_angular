be0a0089b53d0635a895de26e1eff4013d8dae38
alanls1 <alanlima4123@gmail.com>
2025-06-17 11:09:40 -0300

    feat: include the button component

--- src/app/features/feature/feature.component.ts
+++ src/app/features/feature/feature.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../share/button/button.component';
 
 @Component({
   selector: 'app-feature',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './feature.component.html',
-  styleUrl: './feature.component.scss'
+  styleUrl: './feature.component.scss',
 })
-export class FeatureComponent {
-
-}
+export class FeatureComponent {}
